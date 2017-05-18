c70c9b907d3597bc83e6f9aa946f3cd46c2964c8
Ferran Rodenas <frodenas@gmail.com>
2017-05-18 11:35:05 +0200

    Generate and fill system & apps rsa certificates

--- ci/tasks/configure-product.sh
+++ ci/tasks/configure-product.sh
@@ -13,6 +13,62 @@ if [[ "${opsmgr_skip_ssl_verification}X" != "X" ]]; then
   skip_ssl="--skip-ssl-validation"
 fi
 
+# Get Elastic Runtime product ID
+ert_guid=$(om --target ${opsmgr_url} \
+  --skip-ssl-validation \
+  --username "${opsmgr_username}" \
+  --password "${opsmgr_password}" \
+  curl \
+  --silent \
+  --path /api/v0/deployed/products | \
+  jq -r '.[] | select(.type == "cf") | .guid')
+
+# Get Elastic Runtime System Domain
+ert_system_domain=$(om --target ${opsmgr_url} \
+  --skip-ssl-validation \
+  --username "${opsmgr_username}" \
+  --password "${opsmgr_password}" \
+  curl \
+  --silent \
+  --path /api/v0/staged/products/${ert_guid}/properties | \
+  jq -r '.properties[".cloud_controller.system_domain"].value')
+
+# Get Elastic Runtime Apps Domain
+ert_apps_domain=$(om --target ${opsmgr_url} \
+  --skip-ssl-validation \
+  --username "${opsmgr_username}" \
+  --password "${opsmgr_password}" \
+  curl \
+  --silent \
+  --path /api/v0/staged/products/${ert_guid}/properties | \
+  jq -r '.properties[".cloud_controller.apps_domain"].value')
+
+# Generate System Domain SSL certificates
+ert_system_domain_ssl_rsa_certificate=$(om --target ${opsmgr_url} \
+  --skip-ssl-validation \
+  --username "${opsmgr_username}" \
+  --password "${opsmgr_password}" \
+  curl \
+  --silent \
+  --request POST \
+  --path /api/v0/certificates/generate \
+  --data '{ "domains": ["*.${ert_system_domain}"] }')
+ert_system_domain_ssl_cert=$(echo ${ert_system_domain_ssl_rsa_certificate} | jq -r '.certificate')
+ert_system_domain_ssl_key=$(echo ${ert_system_domain_ssl_rsa_certificate} | jq -r '.key')
+
+# Generate Apps Domain SSL certificates
+ert_apps_domain_ssl_rsa_certificate=$(om --target ${opsmgr_url} \
+  --skip-ssl-validation \
+  --username "${opsmgr_username}" \
+  --password "${opsmgr_password}" \
+  curl \
+  --silent \
+  --request POST \
+  --path /api/v0/certificates/generate \
+  --data '{ "domains": ["*.${ert_apps_domain}"] }')
+ert_apps_domain_ssl_cert=$(echo ${ert_apps_domain_ssl_rsa_certificate} | jq -r '.certificate')
+ert_apps_domain_ssl_key=$(echo ${ert_apps_domain_ssl_rsa_certificate} | jq -r '.key')
+
 # Update product networks
 networks_json_file="tile/ci/product/networks.json"
 perl -pi -e "s|{{opsmgr_default_az}}|${opsmgr_default_az}|g" ${networks_json_file}
@@ -23,7 +79,10 @@ echo ${product_networks}
 
 # Update product properties
 properties_json_file="tile/ci/product/properties.json"
-# TODO: Add properties
+perl -pi -e "s|{{ert_system_domain_ssl_cert}}|${ert_system_domain_ssl_cert}|g" ${properties_json_file}
+perl -pi -e "s|{{ert_system_domain_ssl_key}}|${ert_system_domain_ssl_key}|g" ${properties_json_file}
+perl -pi -e "s|{{ert_apps_domain_ssl_cert}}|${ert_apps_domain_ssl_cert}|g" ${properties_json_file}
+perl -pi -e "s|{{ert_apps_domain_ssl_key}}|${ert_apps_domain_ssl_key}|g" ${properties_json_file}
 product_properties=$(cat ${properties_json_file})
 echo "* Properties:"
 echo ${product_properties}
