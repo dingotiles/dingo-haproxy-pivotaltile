36b36f2cc7ed846f742f12d273cc5a0faed07db3
Ferran Rodenas <frodenas@gmail.com>
2017-05-18 15:56:39 +0200

    Fix configure product

--- ci/tasks/configure-product.sh
+++ ci/tasks/configure-product.sh
@@ -52,7 +52,7 @@ ert_system_domain_ssl_rsa_certificate=$(om --target ${opsmgr_url} \
   --silent \
   --request POST \
   --path /api/v0/certificates/generate \
-  --data '{ "domains": ["*.${ert_system_domain}"] }')
+  --data "{ \"domains\": [\"*.${ert_system_domain}\", \"*.login.${ert_system_domain}\", \"*.uaa.${ert_system_domain}\"] }")
 ert_system_domain_ssl_cert=$(echo ${ert_system_domain_ssl_rsa_certificate} | jq -r '.certificate')
 ert_system_domain_ssl_key=$(echo ${ert_system_domain_ssl_rsa_certificate} | jq -r '.key')
 
@@ -65,7 +65,7 @@ ert_apps_domain_ssl_rsa_certificate=$(om --target ${opsmgr_url} \
   --silent \
   --request POST \
   --path /api/v0/certificates/generate \
-  --data '{ "domains": ["*.${ert_apps_domain}"] }')
+  --data "{ \"domains\": [\"*.${ert_apps_domain}\"] }")
 ert_apps_domain_ssl_cert=$(echo ${ert_apps_domain_ssl_rsa_certificate} | jq -r '.certificate')
 ert_apps_domain_ssl_key=$(echo ${ert_apps_domain_ssl_rsa_certificate} | jq -r '.key')
 
@@ -79,14 +79,20 @@ echo ${product_networks}
 
 # Update product properties
 properties_json_file="tile/ci/product/properties.json"
-perl -pi -e "s|{{ert_system_domain_ssl_cert}}|${ert_system_domain_ssl_cert}|g" ${properties_json_file}
-perl -pi -e "s|{{ert_system_domain_ssl_key}}|${ert_system_domain_ssl_key}|g" ${properties_json_file}
-perl -pi -e "s|{{ert_apps_domain_ssl_cert}}|${ert_apps_domain_ssl_cert}|g" ${properties_json_file}
-perl -pi -e "s|{{ert_apps_domain_ssl_key}}|${ert_apps_domain_ssl_key}|g" ${properties_json_file}
+jq --arg ert_system_domain_ssl_cert "${ert_system_domain_ssl_cert}" \
+    --arg ert_system_domain_ssl_key "${ert_system_domain_ssl_key}" \
+    --arg ert_apps_domain_ssl_cert "${ert_apps_domain_ssl_cert}" \
+    --arg ert_apps_domain_ssl_key "${ert_apps_domain_ssl_key}" \
+    '.[".haproxy.system_ssl_rsa_certificate"].value.cert_pem=$ert_system_domain_ssl_cert |
+     .[".haproxy.system_ssl_rsa_certificate"].value.private_key_pem=$ert_system_domain_ssl_key |
+     .[".haproxy.apps_ssl_rsa_certificate"].value.cert_pem=$ert_apps_domain_ssl_cert |
+     .[".haproxy.apps_ssl_rsa_certificate"].value.private_key_pem=$ert_apps_domain_ssl_key' \
+    ${properties_json_file} > ${properties_json_file}.temp && mv ${properties_json_file}.temp ${properties_json_file}
 product_properties=$(cat ${properties_json_file})
 echo "* Properties:"
 echo ${product_properties}
 
+# Configure product
 om --target ${opsmgr_url} \
    ${skip_ssl} \
    --username "${opsmgr_username}" \
